dc3d371da36b5bb65eb626192c7e5d66dad35c2d
bidis <nikcharg4@gmail.com>
2024-03-19 10:30:42 +0200

    Make changes on controllers and db

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,13 @@
 const Product = require('../models/Product');
 const { StatusCodes } = require('http-status-codes');
 
+//create Product
 const createProduct = async (req, res) => {
   const product = await Product.create(req.body);
   res.status(StatusCodes.CREATED).json({ product });
 };
+
+//get all products 
 const getAllProducts = async (req, res) => {
   const products = await Product.find({});
   res.status(StatusCodes.OK).json({ products });
--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
+//connerct to mongo db 
 const connectDB = (url) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
