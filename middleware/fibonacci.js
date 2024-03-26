192e4e051f80a41ef0bfd2b9dcecec1956c40c4c
nikos-charalampidis <141315582+nikos-charalampidis@users.noreply.github.com>
2024-03-26 16:15:56 +0200

    Create fibonacci.js

---
+++ middleware/fibonacci.js
@@ -0,0 +1,11 @@
+const fibonacci = (n) => {
+  const sequence = [0, 1];
+  
+  for (let i = 2; i < n; i++) {
+    sequence[i] = sequence[i - 1] + sequence[i - 2];
+  }
+  
+  return sequence;
+}
+
+module.exports = fibonacci
