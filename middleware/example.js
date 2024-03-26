97828d6512d6346eaaea0cfa2b1b5252f0d06829
bidis <nikcharg4@gmail.com>
2024-03-26 16:11:28 +0200

    Added logs

--- middleware/example.js
+++ middleware/example.js
@@ -1,6 +1,8 @@
 const counter = (num) => {
     for(let i=0; i<num; i++){
+        console.log('This is a counter');
         console.log(`OII ${i}`);
+        console.log('The counter finished its usage');
     }
 }
 
