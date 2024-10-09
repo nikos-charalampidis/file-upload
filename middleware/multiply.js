fc13c0160bdfe07dc1f89e359fd0e79548e6d6aa
bidis <nikcharg4@gmail.com>
2024-10-09 11:53:20 +0300

    Add multiply

---
+++ middleware/multiply.js
@@ -0,0 +1,13 @@
+function arrayMultiplication(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+      throw new Error('Arrays must have the same length');
+    }
+  
+    const result = [];
+    for (let i = 0; i < arr1.length; i++) {
+      result.push(arr1[i] * arr2[i]);
+    }
+    return result;
+  }
+
+module.exports = arrayMultiplication
