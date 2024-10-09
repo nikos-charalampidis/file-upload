630825801b468aabdf61664f50ecf8cc3b7309bd
bidis <nikcharg4@gmail.com>
2024-10-09 12:06:43 +0300

    Add duplicate code

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -14,6 +14,32 @@ const auth = async (req, res, next) => {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
     // attach the user to the job routes
     req.user = { userId: payload.userId, name: payload.name }
+    console.log("oi");
+    console.log("oi");
+    console.log("oi");
+    console.log("oi");
+    next()
+  } catch (error) {
+    throw new UnauthenticatedError('Authentication invalid')
+  }
+}
+
+const auth2 = async (req, res, next) => {
+  // check header
+  const authHeader = req.headers.authorization
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
+    throw new UnauthenticatedError('Authentication invalid')
+  }
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET)
+    // attach the user to the job routes
+    req.user = { userId: payload.userId, name: payload.name }
+    console.log("oi");
+    console.log("oi");
+    console.log("oi");
+    console.log("oi");
     next()
   } catch (error) {
     throw new UnauthenticatedError('Authentication invalid')
