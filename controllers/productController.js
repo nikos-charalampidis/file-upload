ce0d5c23bdbf813bac922648171e7cd6dd1a7276
nikos-charalampidis <141315582+nikos-charalampidis@users.noreply.github.com>
2024-11-05 09:36:51 +0200

    test

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,8 @@
 const Product = require('../models/Product');
 const { StatusCodes } = require('http-status-codes');
 
+const a;
+
 //create Product
 const createProduct = async (req, res) => {
   const product = await Product.create(req.body);
