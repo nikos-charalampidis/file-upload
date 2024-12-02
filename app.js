221f7d6433f0fdda1415bf7dd8678038dc7e1429
bidis <nikcharg4@gmail.com>
2024-12-02 16:32:24 +0200

    dummy

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
+      console.log(`Server is now listening on port ${port}...`)
     );
   } catch (error) {
     console.log(error);
