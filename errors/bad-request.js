c2b7f12862c9bfda2bad9da82df4e4cd85e41a62
nikos-charalampidis <141315582+nikos-charalampidis@users.noreply.github.com>
2025-04-15 16:35:08 +0300

    Update bad-request.js

--- errors/bad-request.js
+++ errors/bad-request.js
@@ -1,6 +1,8 @@
 const { StatusCodes } = require('http-status-codes');
 const CustomAPIError = require('./custom-api');
 
+const unused = "nikos";
+
 class BadRequestError extends CustomAPIError {
   constructor(message) {
     super(message);
