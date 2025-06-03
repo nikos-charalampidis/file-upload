65f337e72b9cb8a32b890dad6131641cc027375a
nikos-charalampidis <141315582+nikos-charalampidis@users.noreply.github.com>
2025-06-03 16:43:17 +0300

    Import some violations

--- errors/not-found.js
+++ errors/not-found.js
@@ -1,6 +1,9 @@
 const { StatusCodes } = require('http-status-codes');
 const CustomAPIError = require('./custom-api');
 
+const errorA = "i am an error";
+const errorB = "i am another error";
+
 class NotFoundError extends CustomAPIError {
   constructor(message) {
     super(message);
